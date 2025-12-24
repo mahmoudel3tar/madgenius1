f80419a1272e4abd12498285d91ead2ec4d1b235
mahmoudel3tar <142107237+mahmoudel3tar@users.noreply.github.com>
2025-12-24 13:01:48 +0200

    Reduce padding in main container

--- app/page.tsx
+++ app/page.tsx
@@ -117,7 +117,7 @@ We Know Technology Well <br/> Our staff is a blend of fine young men and woman w
       </main>
     </div>
        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-3 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-3 px-3 bg-white dark:bg-black sm:items-start">
         <Image
           className=""
           src="/1.jpg"
